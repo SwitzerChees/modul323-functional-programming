a373e8f335dfc63db9023dc50b6bedf46afed88a
switzerchees <patrickmichel96@yahoo.de>
2024-08-29 15:47:22 +0200

    Fix weak comparison

--- function_compositions.js
+++ function_compositions.js
@@ -2,7 +2,7 @@ const R = require('./ramda.min.js')
 
 const sentence =
   'PechaKucha is a presentation style in which 20 slides are shown for 20 seconds each (6 minutes and 40 seconds in total).'
-const filterNumbers = (chars) => chars.filter((c) => c != ' ' && !isNaN(c))
+const filterNumbers = (chars) => chars.filter((c) => c !== ' ' && !isNaN(c))
 
 const countNumChars = R.compose(R.length, filterNumbers, R.split)
 
